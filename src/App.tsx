962283017fb3fc170f211c55b4d4e7c52a628cf4
Minje Sung <minje813@dgu.ac.kr>
2025-06-13 14:36:20 +0900

    chore: emotion styled 설치

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
+import styled from '@emotion/styled';
 import './App.css';
 import { motion } from 'motion/react';
 
 function App() {
   return (
     <>
-      <motion.div style={{ background: '#ff0088' }} animate={{ rotate: 360 }}>
-        회전 확인
-      </motion.div>
+      <S.Container animate={{ rotate: 360 }}>회전 확인</S.Container>
     </>
   );
 }
 
 export default App;
+
+const S = {
+  Container: styled(motion.div)`
+    background-color: #ff0088;
+  `,
+};
