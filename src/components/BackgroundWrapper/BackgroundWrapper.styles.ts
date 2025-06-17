bc4b1aa1a9d5cd44dd9956190b7ef13837696ac5
deokbin <deokbin@naver.com>
2025-06-17 15:46:20 +0900

    style: Wrapper 크기 지정 및 display 설정

--- src/components/BackgroundWrapper/BackgroundWrapper.styles.ts
+++ src/components/BackgroundWrapper/BackgroundWrapper.styles.ts
@@ -2,4 +2,6 @@ import styled from "@emotion/styled";
 export const Wrapper = styled.div<{ background: string }>`
   background-color: ${({ background }) => background};
   min-height: 100vh;
+  display: flex;
+  justify-content: center;
 `;
---
+++ src/components/PlanetBody/PlanetBody.styles.ts
@@ -0,0 +1,7 @@
+import styled from "@emotion/styled";
+
+export const PlanetBodyWrapper = styled.div`
+  width: 400px;
+  height: 400px;
+  position: relative;
+`;
