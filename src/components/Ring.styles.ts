4b1e792d8da2f2f21b7db0d5ee3b3c968c4445ee
deokbin <deokbin@naver.com>
2025-06-24 00:55:45 +0900

    style: props로 고리 색 변경 가능하도록 설정

--- src/components/Ring.styles.ts
+++ src/components/Ring.styles.ts
@@ -1,6 +1,5 @@
 import styled from "@emotion/styled";
 
-//Todo: props로 색상 변경 가능하게 하기
-export const StyledRing = styled.svg`
-  color: #e3e3e3;
+export const StyledRing = styled.svg<{ ringColor: string }>`
+  color: ${(props) => props.ringColor};
 `;
