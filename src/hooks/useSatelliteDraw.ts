8737220851b42e236a9a4f77653cf78f15114725
deokbin <deokbin@naver.com>
2025-06-26 21:28:20 +0900

    feat: 다 모으면 마지막에 스페셜 행성이 뽑히도록 기능 추가

--- src/hooks/useSatelliteDraw.ts
+++ src/hooks/useSatelliteDraw.ts
@@ -3,17 +3,28 @@ import { useStorageContext } from "../contexts/StorageContext/useStorageContext"
 import { SatelliteData } from "../data/SatelliteData";
 import getNewSatellite from "../utils/getNewSatellite";
 
+const SPECIAL_SATELLITE_ID = 15;
+
 const useSatelliteDraw = () => {
   const { token, spendToken, satelliteList, addSatelliteIdList } =
     useStorageContext();
 
   const satelliteIdSet = new Set(satelliteList.map(({ id }) => id));
 
-  const canDraw = token >= DRAW_PRICE;
+  const canDraw =
+    token >= DRAW_PRICE && satelliteIdSet.size < SatelliteData.length;
 
   const handleDrawButton = () => {
     spendToken(DRAW_PRICE);
     console.log("hello");
+
+    if (
+      satelliteIdSet.size === SatelliteData.length - 1 &&
+      !satelliteIdSet.has(SPECIAL_SATELLITE_ID)
+    ) {
+      addSatelliteIdList(SPECIAL_SATELLITE_ID);
+      return;
+    }
 
     let newSatellite;
     while (true) {
