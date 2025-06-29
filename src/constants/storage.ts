e46235907f1aa479bdb540990d8b3da9b4d6cef9
deokbin <deokbin@naver.com>
2025-06-29 21:19:31 +0900

    chore: bookmarkOpen 로컬 스토리지 키 상수로 추가

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -1,3 +1,4 @@
 export const TOKEN_STORAGE_KEY = "token";
 export const SATELLITE_ID_LIST_KEY = "inventory";
 export const SELECTED_SATELLITE_ID_KEY = "selected";
+export const IS_BOOKMARK_OPEN_KEY = "isBookmarkOpen";
