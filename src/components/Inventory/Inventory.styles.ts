378ff99ea1195c31f1fb4e24afc92af45e7ca636
Minje Sung <minje813@dgu.ac.kr>
2025-06-29 23:51:29 +0900

    feat: Satellite 페이지네이션 구현

--- src/components/Inventory/Inventory.styles.ts
+++ src/components/Inventory/Inventory.styles.ts
@@ -14,11 +14,17 @@ export const ItemsWrapper = styled(motion.div)`
   max-width: 100%;
 `;
 interface ItemBoxProps {
+  iconSize: number;
   isSelected: boolean;
   isDarkMode?: boolean;
 }
 
 export const ItemBox = styled(motion.div)<ItemBoxProps>`
+  width: ${({ iconSize }) => iconSize}px;
+  height: ${({ iconSize }) => iconSize}px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
   font-size: 30px;
   padding: 10px 8px;
   border-radius: 6px;
@@ -40,4 +46,30 @@ export const ItemBox = styled(motion.div)<ItemBoxProps>`
       return isDarkMode ? "rgba(255, 255, 255, 0.05)" : "rgba(0, 0, 0, 0.05)";
     }};
   }
+`;
+
+interface PaginationContainer {
+  iconSize: number;
+  rowLength: number;
+}
+
+export const PaginationContainer = styled.div<PaginationContainer>`
+  width: ${({ rowLength }) => rowLength}px;
+  height: ${({ iconSize }) => iconSize}px;
+  overflow: hidden;
+`;
+
+export const RowContainer = styled(motion.div)<{ startX: number }>`
+  display: flex;
+  gap: 10px;
+  align-items: center;
+  padding: 0 5px;
+  transform: translateX(-${({ startX }) => startX}px);
+`;
+
+export const ItemRow = styled(motion.div)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  gap: 10px;
 `;
--- src/components/Inventory/Inventory.tsx
+++ src/components/Inventory/Inventory.tsx
@@ -2,15 +2,35 @@ import { useState } from "react";
 import InventoryButton from "../IconButton/InventoryButton/InventoryButton";
 import { AnimatePresence } from "motion/react";
 import * as S from "./Inventory.styles";
-import type { InventoryProps } from "./InventoryTypes";
+import type { InventoryProps, Satellite } from "./InventoryTypes";
+import IconButton from "../IconButton/IconButton";
 
 export default function Inventory({
   inventory,
   selected,
   setSelected,
   isDarkMode = false,
+  amount = 8,
 }: InventoryProps) {
+  const [page, setPage] = useState(0);
   const [showItems, setShowItems] = useState(false);
+
+  const separatedItems = inventory.reduce<Satellite[][]>((acc, item, index) => {
+    const chunkIndex = Math.floor(index / amount);
+    if (!acc[chunkIndex]) {
+      acc[chunkIndex] = [];
+    }
+    acc[chunkIndex].push(item);
+    return acc;
+  }, []);
+
+  const movePrevPage = () => {
+    setPage((prev) => Math.max(0, prev - 1));
+  };
+
+  const moveNextPage = () => {
+    setPage((prev) => Math.min(separatedItems.length - 1, prev + 1));
+  };
 
   const toggleInventory = () => {
     setShowItems((prev) => !prev);
@@ -37,25 +57,90 @@ export default function Inventory({
             exit={{ opacity: 0, x: -20 }}
             transition={{ duration: 0.3 }}
           >
-            {inventory.map((item) => (
-              <S.ItemBox
-                key={item.id}
-                layout
-                layoutId={`item-${item.id}`}
-                isSelected={selected === item.id}
-                isDarkMode={isDarkMode}
-                initial={{ opacity: 0, x: -20 }}
-                animate={{ opacity: 1, x: 0 }}
-                exit={{ opacity: 0, x: -20 }}
-                transition={{ duration: 0.3 }}
-                onClick={() => handleSelect(item.id)}
-              >
-                {item.icon}
-              </S.ItemBox>
-            ))}
+            <SatelliteItemPagination
+              page={page}
+              movePrevPage={movePrevPage}
+              moveNextPage={moveNextPage}
+              amount={amount}
+              separatedItems={separatedItems}
+              selected={selected}
+              handleSelect={handleSelect}
+              isDarkMode={isDarkMode}
+            />
           </S.ItemsWrapper>
         )}
       </AnimatePresence>
     </S.InventoryWrapper>
   );
 }
+
+interface SatelliteItemPaginationProps {
+  page: number;
+  movePrevPage: () => void;
+  moveNextPage: () => void;
+  separatedItems: Satellite[][];
+  amount: number;
+  selected: number | null;
+  handleSelect: (id: number) => void;
+  isDarkMode?: boolean;
+}
+
+const SatelliteItemPagination = ({
+  page,
+  movePrevPage,
+  moveNextPage,
+  separatedItems,
+  amount,
+  selected,
+  handleSelect,
+  isDarkMode = false,
+}: SatelliteItemPaginationProps) => {
+  const ICON_SIZE = 50;
+  const rowLength = ICON_SIZE * amount + 10 * amount;
+
+  return (
+    <>
+      <IconButton
+        name="dice"
+        size={ICON_SIZE}
+        onClick={movePrevPage}
+        isDarkMode={isDarkMode}
+        disabled={page === 0}
+      />
+
+      <S.PaginationContainer iconSize={ICON_SIZE} rowLength={rowLength}>
+        <S.RowContainer startX={rowLength * page}>
+          {separatedItems.map((separatedItem) => (
+            <S.ItemRow
+              initial={{ opacity: 0, x: -20 }}
+              animate={{ opacity: 1, x: 0 }}
+              exit={{ opacity: 0, x: -20 }}
+              transition={{ duration: 0.3 }}
+            >
+              {separatedItem.map((item) => (
+                <S.ItemBox
+                  key={item.id}
+                  iconSize={ICON_SIZE}
+                  layout
+                  layoutId={`item-${item.id}`}
+                  isSelected={selected === item.id}
+                  isDarkMode={isDarkMode}
+                  onClick={() => handleSelect(item.id)}
+                >
+                  {item.icon}
+                </S.ItemBox>
+              ))}
+            </S.ItemRow>
+          ))}
+        </S.RowContainer>
+      </S.PaginationContainer>
+      <IconButton
+        name="dice"
+        size={50}
+        onClick={moveNextPage}
+        isDarkMode={isDarkMode}
+        disabled={page === separatedItems.length - 1}
+      />
+    </>
+  );
+};
